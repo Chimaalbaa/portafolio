e14825b0e5c6b3bcb06f0170b1b6e12b370d22ac
VicAlba <66865356+Chimaalbaa@users.noreply.github.com>
2024-06-24 21:31:51 -0600

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -1,10 +1,12 @@
+/*Autor: Victor Emanuel Alba Reyes*/
 #include <stdlib.h>
 
 struct nodo{
-    int valor;
+    int *dir_actual;
 };
 
 struct arista{
+    int *apu_sig, *apu_ant;
     int km;
     int precio;
 };
